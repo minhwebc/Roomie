1d9a716ffd2612ed02eb35a8e1bc82a485ef588e
muhaamed <mold81193@gmail.com>
2017-05-18 02:17:41 -0700

    home screen not hiding behind navBar

--- Roomie/HomeViewController.swift
+++ Roomie/HomeViewController.swift
@@ -9,10 +9,49 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    //////////////////////////////////////////
+    /////////////////////////
+    ////// SET UP THE BUTTONS TO CLICK AND GO TO OTHER VIEWS
+    let ButtonsContainer:UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+        
+    }()
+    
+    func setupButtonsContainer() {
+        // contraint inputContainer
+        ButtonsContainer.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        ButtonsContainer.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        ButtonsContainer.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        ButtonsContainer.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
 
+    }
+
+    
+    let choreButton:UIButton = {
+        var button = UIButton()
+        button.backgroundColor = UIColor.red
+        button.setTitle("Chores", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    func setupChoreButton() {
+        choreButton.leftAnchor.constraint(equalTo: ButtonsContainer.leftAnchor).isActive = true
+        choreButton.topAnchor.constraint(equalTo: ButtonsContainer.topAnchor).isActive = true
+        choreButton.widthAnchor.constraint(equalTo: ButtonsContainer.widthAnchor, multiplier: 1/2).isActive = true
+        choreButton.heightAnchor.constraint(equalTo: ButtonsContainer.heightAnchor, multiplier: 1/3).isActive = true
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.edgesForExtendedLayout = []
         view.backgroundColor = UIColor.blue
+        view.addSubview(ButtonsContainer)
+        setupButtonsContainer()
+        ButtonsContainer.addSubview(choreButton)
+        setupChoreButton()
         // Do any additional setup after loading the view.
     }
 
