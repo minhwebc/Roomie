a96ae9d5f32d53f54f6e39b47fd28c56b1a7b8cb
muhaamed <mold81193@gmail.com>
2017-05-23 18:33:14 -0700

    code cleaned and sectioned

--- Roomie/LoginViewController.swift
+++ Roomie/LoginViewController.swift
@@ -10,35 +10,13 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    let loginSignup:UISegmentedControl = {
-        let items = ["Login", "Register"]
-        let seg = UISegmentedControl(items: items)
-        seg.selectedSegmentIndex = 0
-        //seg.backgroundColor = UIColor.blue
-        seg.tintColor = UIColor.white
-        seg.layer.cornerRadius = 10
-        seg.translatesAutoresizingMaskIntoConstraints = false
-        return seg
-    }()
-    
-    func setupSegmentedControl() {
-        loginSignup.addTarget(self, action: #selector (handleLoginRegisterChange), for: .valueChanged)
-        loginSignup.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        loginSignup.bottomAnchor.constraint(equalTo: inputContainer.topAnchor, constant: -12).isActive = true
-        loginSignup.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
-        loginSignup.heightAnchor.constraint(equalTo: loginButton.heightAnchor, multiplier: 1/2).isActive = true
-    }
-    
-    func handleLoginRegisterChange()  {
-        let title = loginSignup.titleForSegment(at: loginSignup.selectedSegmentIndex)
-        loginButton.setTitle(title, for: .normal)
-    }
+    ///////////////////
+    //////
+    /// Defining elements in the login screen
     
     let inputContainer:UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.clear
-        //self.userEmailTextField.layer.mask = rectShape
-        //self.userEmailTextField.layer.backgroundColor = UIColor.clear.cgColor
         view.layer.cornerRadius = 10
         view.layer.masksToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -46,24 +24,15 @@ class LoginViewController: UIViewController {
         
     }()
     
-    func setupInputContainer() {
-        // contraint inputContainer
-        inputContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        inputContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        inputContainer.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
-        inputContainer.heightAnchor.constraint(equalToConstant: 150).isActive = true
-        inputContainer.addSubview(name)
-        setupName()
-        inputContainer.addSubview(email)
-        setupEmail()
-        inputContainer.addSubview(password)
-        setupPassword()
-        inputContainer.addSubview(nameSeparator)
-        setupNameSeparator()
-        inputContainer.addSubview(emailSeparator)
-        setupEmailSeparator()
-        
-    }
+    let loginSignup:UISegmentedControl = {
+        let items = ["Login", "Register"]
+        let seg = UISegmentedControl(items: items)
+        seg.selectedSegmentIndex = 0
+        seg.tintColor = UIColor.white
+        seg.layer.cornerRadius = 10
+        seg.translatesAutoresizingMaskIntoConstraints = false
+        return seg
+    }()
     
     let name:UITextField = {
         let name = UITextField()
@@ -74,13 +43,6 @@ class LoginViewController: UIViewController {
         return name
     }()
     
-    func setupName() {
-        name.leftAnchor.constraint(equalTo: inputContainer.leftAnchor, constant: 12).isActive = true
-        name.topAnchor.constraint(equalTo: inputContainer.topAnchor).isActive = true
-        name.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
-        name.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 1/3).isActive = true
-    }
-    
     let nameSeparator:UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.lightGray
@@ -88,17 +50,25 @@ class LoginViewController: UIViewController {
         return view
     }()
     
-    func setupNameSeparator() {
-        nameSeparator.leftAnchor.constraint(equalTo: inputContainer.leftAnchor).isActive = true
-        nameSeparator.topAnchor.constraint(equalTo: name.bottomAnchor).isActive = true
-        nameSeparator.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
-        nameSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
-    }
+    let groupName:UITextField = {
+        let groupName = UITextField()
+        groupName.attributedPlaceholder = NSAttributedString(string:"Group Name", attributes:[NSForegroundColorAttributeName: UIColor.lightGray])
+        groupName.tintColor = .white
+        groupName.textColor = UIColor.white
+        groupName.translatesAutoresizingMaskIntoConstraints = false
+        return groupName
+    }()
     
+    let groupNameSeparator:UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     let email:UITextField = {
         let email = UITextField()
-       // email.placeholder = "Email"
+        // email.placeholder = "Email"
         email.attributedPlaceholder = NSAttributedString(string:"Email", attributes:[NSForegroundColorAttributeName: UIColor.lightGray])
         email.tintColor = .white
         email.textColor = .white
@@ -106,29 +76,12 @@ class LoginViewController: UIViewController {
         return email
     }()
     
-    func setupEmail() {
-        // email Contraints
-        email.leftAnchor.constraint(equalTo: inputContainer.leftAnchor, constant: 12).isActive = true
-        email.topAnchor.constraint(equalTo: name.bottomAnchor).isActive = true
-        email.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
-        email.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 1/3).isActive = true
-        
-    }
-    
     let emailSeparator:UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.lightGray
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    
-    func setupEmailSeparator() {
-        emailSeparator.leftAnchor.constraint(equalTo: inputContainer.leftAnchor).isActive = true
-        emailSeparator.topAnchor.constraint(equalTo: email.bottomAnchor).isActive = true
-        emailSeparator.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
-        emailSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
-    }
-    
     
     let password:UITextField = {
         let pass = UITextField()
@@ -139,13 +92,6 @@ class LoginViewController: UIViewController {
         pass.isSecureTextEntry = true
         return pass
     }()
-    
-    func setupPassword() {
-        password.leftAnchor.constraint(equalTo: inputContainer.leftAnchor, constant: 12).isActive = true
-        password.topAnchor.constraint(equalTo: email.bottomAnchor).isActive = true
-        password.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
-        password.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 1/3).isActive = true
-    }
     
     let loginButton:UIButton = {
         var button = UIButton()
@@ -160,6 +106,109 @@ class LoginViewController: UIViewController {
         return button
     }()
     
+    ///////////////////
+    //////
+    /// Constraining elements in the login screen
+    
+    func setupSegmentedControl() {
+        loginSignup.addTarget(self, action: #selector (handleLoginRegisterChange), for: .valueChanged)
+        loginSignup.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loginSignup.bottomAnchor.constraint(equalTo: inputContainer.topAnchor, constant: -12).isActive = true
+        loginSignup.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        loginSignup.heightAnchor.constraint(equalTo: loginButton.heightAnchor, multiplier: 1/2).isActive = true
+    }
+    
+    func setupInputContainer() {
+        // contraint inputContainer
+        inputContainer.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputContainer.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputContainer.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputContainer.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        // added elements to the input box
+        inputContainer.addSubview(name)
+        inputContainer.addSubview(groupName)
+        inputContainer.addSubview(email)
+        inputContainer.addSubview(password)
+        inputContainer.addSubview(nameSeparator)
+        inputContainer.addSubview(groupNameSeparator)
+        inputContainer.addSubview(emailSeparator)
+        
+        // functions to add constraints to elements in the input box
+        setupName()
+        setupGroupName()
+        setupEmail()
+        setupPassword()
+        setupNameSeparator()
+        setupGroupNameSeparator()
+        setupEmailSeparator()
+    }
+    
+    func setupName() {
+        name.leftAnchor.constraint(equalTo: inputContainer.leftAnchor, constant: 12).isActive = true
+        name.topAnchor.constraint(equalTo: inputContainer.topAnchor).isActive = true
+        name.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        name.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 1/4).isActive = true
+    }
+    
+    
+    
+    func setupNameSeparator() {
+        nameSeparator.leftAnchor.constraint(equalTo: inputContainer.leftAnchor).isActive = true
+        nameSeparator.topAnchor.constraint(equalTo: name.bottomAnchor).isActive = true
+        nameSeparator.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        nameSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    
+    
+    func setupGroupName() {
+        groupName.leftAnchor.constraint(equalTo: inputContainer.leftAnchor, constant: 12).isActive = true
+        groupName.topAnchor.constraint(equalTo: name.bottomAnchor).isActive = true
+        groupName.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        groupName.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 1/4).isActive = true
+    }
+    
+    
+    
+    func setupGroupNameSeparator() {
+        groupNameSeparator.leftAnchor.constraint(equalTo: inputContainer.leftAnchor).isActive = true
+        groupNameSeparator.topAnchor.constraint(equalTo: groupName.bottomAnchor).isActive = true
+        groupNameSeparator.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        groupNameSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    
+    
+    
+    func setupEmail() {
+        // email Contraints
+        email.leftAnchor.constraint(equalTo: inputContainer.leftAnchor, constant: 12).isActive = true
+        email.topAnchor.constraint(equalTo: groupName.bottomAnchor).isActive = true
+        email.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        email.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 1/4).isActive = true
+        
+    }
+    
+    
+    
+    func setupEmailSeparator() {
+        emailSeparator.leftAnchor.constraint(equalTo: inputContainer.leftAnchor).isActive = true
+        emailSeparator.topAnchor.constraint(equalTo: email.bottomAnchor).isActive = true
+        emailSeparator.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        emailSeparator.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+    
+    
+    
+    
+    func setupPassword() {
+        password.leftAnchor.constraint(equalTo: inputContainer.leftAnchor, constant: 12).isActive = true
+        password.topAnchor.constraint(equalTo: email.bottomAnchor).isActive = true
+        password.widthAnchor.constraint(equalTo: inputContainer.widthAnchor).isActive = true
+        password.heightAnchor.constraint(equalTo: inputContainer.heightAnchor, multiplier: 1/4).isActive = true
+    }
+    
     func setupLoginButton() {
         loginButton.leftAnchor.constraint(equalTo: inputContainer.leftAnchor).isActive = true
         loginButton.topAnchor.constraint(equalTo: inputContainer.bottomAnchor, constant: 12).isActive = true
@@ -167,12 +216,20 @@ class LoginViewController: UIViewController {
         loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
+    ///////////////////
+    //////
+    /// Functions to handle button clicks and segmented control .valueChanged
+    /// Also includes function to add properties to the textFields
+    
+    func handleLoginRegisterChange()  {
+        let title = loginSignup.titleForSegment(at: loginSignup.selectedSegmentIndex)
+        loginButton.setTitle(title, for: .normal)
+    }
+    
     func loggedIn() {
-        
         let objVC: HomeViewController? = HomeViewController()
-        let navController = UINavigationController(rootViewController: objVC!) // Creating a navigation controller with VC1 at the root of the navigation stack.
+        let navController = UINavigationController(rootViewController: objVC!)
         self.present(navController, animated:true, completion: nil)
-        
     }
     
     override func viewDidLoad() {
