d4e2e3c2c27401f09afad3515c2e2a49e2d09e9e
muhaamed <mold81193@gmail.com>
2017-05-26 01:51:24 -0700

    add chore button added and addchore view added, waiting to be customized and add textfields

--- Roomie/ChoresViewController.swift
+++ Roomie/ChoresViewController.swift
@@ -9,28 +9,95 @@
 import UIKit
 
 class ChoresViewController: UIViewController {
+    
+    let addChoreView:UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.white
+        view.layer.cornerRadius = 30
+        view.layer.opacity = 0.5
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+        
+    }()
+    
+    let cancelButton:UIButton = {
+        var button = UIButton()
+        button.backgroundColor = UIColor.blue
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel!.font = UIFont.boldSystemFont(ofSize: 25)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let saveButton:UIButton = {
+        var button = UIButton()
+        button.backgroundColor = UIColor.blue
+        button.setTitle("Save", for: .normal)
+        button.titleLabel!.font = UIFont.boldSystemFont(ofSize: 25)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    func constrainAddChoreView() {
+        addChoreView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        addChoreView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        addChoreView.heightAnchor.constraint(equalTo: self.view.heightAnchor, constant: -150).isActive = true
+        addChoreView.widthAnchor.constraint(equalTo: self.view.widthAnchor, constant: -80).isActive = true
+        addChoreView.addSubview(cancelButton)
+        addChoreView.addSubview(saveButton)
+        constrainSaveButton()
+        constrainCancelButton()
+    }
+    
+    func constrainSaveButton() {
+        saveButton.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        saveButton.rightAnchor.constraint(equalTo: addChoreView.rightAnchor, constant:-20).isActive = true
+        saveButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        saveButton.bottomAnchor.constraint(equalTo: addChoreView.bottomAnchor, constant: -20).isActive = true
+    }
+    
+    
+    func constrainCancelButton() {
+        cancelButton.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        cancelButton.leftAnchor.constraint(equalTo: addChoreView.leftAnchor,constant:20).isActive = true
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.bottomAnchor.constraint(equalTo: addChoreView.bottomAnchor, constant: -20).isActive = true
+        cancelButton.addTarget(self, action: #selector (handleCancel), for: .touchUpInside)
+    }
+    
+    func addChore()  {
+        print("Hello")
+        self.view.addSubview(addChoreView)
+        constrainAddChoreView()
+        
+    }
+    
+    func handleCancel() {
+        //        UIView.transition(with: self.addChoreView, duration: 0.5, options: UIViewAnimationOptions.,
+        //                          animations: nil, completion: { _ in
+        //        self.addChoreView.removeFromSuperview()
+        //        })
+        self.addChoreView.removeFromSuperview()
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.edgesForExtendedLayout = []
         view.backgroundColor = UIColor(red: 233/255.0, green:92/255.0 , blue: 111/255.0 ,alpha:1)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addChore))
         // Do any additional setup after loading the view.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
