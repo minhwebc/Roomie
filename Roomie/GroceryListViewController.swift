0a60df35e80a0328234d0c402c82397dc86bf0b5
Vicky Juan <vicky37@uw.edu>
2017-06-06 00:40:42 -0700

    addAmount works, need to add completed to the completed tabbar item

--- Roomie/GroceryListViewController.swift
+++ Roomie/GroceryListViewController.swift
@@ -12,16 +12,6 @@ import Firebase
 
 class GroceryListViewController: UIViewController {
     
-    var rootRef: DatabaseReference = Database.database().reference()
-    let sessionManager = SessionManager()
-    var users: [Dictionary<String,String>] = []
-    var groceryID: String?
-    var groceryName: String?
-    var creator: String?
-    var due_on: String?
-    var gvc: GroceriesViewController?
-    
-    
     let addAmountView:UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.white
@@ -90,7 +80,6 @@ class GroceryListViewController: UIViewController {
         constrainAmountTextView()
         constrainCancelButton()
         constrainSaveButton()
-        
     }
     
     func constrainAmountLabel(){
@@ -124,54 +113,38 @@ class GroceryListViewController: UIViewController {
     }
     
     func handleCancel() {
-//        amountTextField.text = ""
         self.addAmountView.removeFromSuperview()
         gvc?.refreshTable()
         self.navigationController?.popViewController(animated: true)
     }
     
-    // function for when a user saves a chore
+    var rootRef: DatabaseReference = Database.database().reference()
+    let sessionManager = SessionManager()
+    var groceryID: String?
+    var groceryName: String?
+    var creator: String?
+    var userID: String?
+    var gvc: GroceriesViewController?
+    
     func handleSave() {
-//        let selectedDate = dateFormatter.string(from: Calendar.current.date(byAdding: .day, value: 1, to: dueDatePicker.date)!)
-//        
-//        if let title = titleTextField.text {
-//            let desc = descTextField.text ?? ""
-//            
-//            let choresRef = rootRef.child("groups/\(sessionManager.getUserDetails()["groupName"]!)/chores")
-//            let key : String = choresRef.childByAutoId().key
-//            choresRef.child("\(key)/title").setValue(title)
-//            choresRef.child("\(key)/description").setValue(desc)
-//            choresRef.child("\(key)/create_on").setValue(dateFormatter.string(from: Date()))
-//            choresRef.child("\(key)/due_on").setValue(selectedDate)
-//            choresRef.child("\(key)/creator").setValue(userName)
-//            choresRef.child("\(key)/creatorID").setValue(sessionManager.getUserDetails()["userID"]!)
-//            print("add chore successfully!")
-//            let dict = ["title":titleTextField.text!,"desc":descTextField.text!,"creator":"Created by: \(userName)","assignee":"Assigned to: ","dueDate":"Due on: \(selectedDate)", "id": "\(key)"]
-//            
-//            chores.append(dict)
-//            refreshTable()
-//        }
-//        else {
-//            print("title field cannot be empty!")
-//        }
+        let id = groceryID!
+        let name = groceryName!
+        let userid = userID!
         
+        let groupRef = rootRef.child("groups/\(sessionManager.getUserDetails()["groupName"]!)")
+        let userRef = groupRef.child("users/\(userid)")
+        
+        groupRef.child("grocery/\(id)/totalAmount").setValue(amountTextField.text)
+        groupRef.child("grocery/\(id)/payID").setValue(userID)
+        
+        userRef.child("grocery/\(id)/title").setValue(name)
+        userRef.child("grocery/\(id)/creator").setValue(creator)
+        userRef.child("grocery/\(id)/amount").setValue(amountTextField.text)
+        
+        self.addAmountView.removeFromSuperview()
+        gvc?.refreshTable()
+        self.navigationController?.popViewController(animated: true)
     }
-
-//        let id = groceryID!
-//        let name = groceryName!
-//        let groupRef = rootRef.child("groups/\(sessionManager.getUserDetails()["groupName"]!)")
-//        let userRef = groupRef.child("users/\(users[indexPath.row]["id"]!)")
-    
-//        ////////////////////check this later
-//        groupRef.child("grocery/\(id)/totalAmount").setValue(users[indexPath.row]["name"]!)
-//        groupRef.child("grocery/\(id)/payID").setValue(users[indexPath.row]["id"]!)
-//        
-//        userRef.child("grocery/\(id)/title").setValue(name)
-//        userRef.child("grocery/\(id)/creator").setValue(creator)
-//        userRef.child("grocery/\(id)/due_on").setValue(due_on)
-//        print("Successfully assign the grocery(id=\(id)) to the user(id=\(users[indexPath.row]["id"]!))!")
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -180,6 +153,7 @@ class GroceryListViewController: UIViewController {
         self.view.addSubview(addAmountView)
         constrainAddAmountView()
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
